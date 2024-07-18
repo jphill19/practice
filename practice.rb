0824a1ee488a7e5b512520be7646017cd212a9bb
John Hill <jphill1989@gmail.com>
2024-07-18 09:40:44 -0600

    Feat: practice -> NewPractice

--- practice.rb
+++ practice.rb
@@ -1,9 +1,10 @@
-class Practice
+class NewPractice
 
-  attr_reader :test_file
+  attr_reader :test_file, :test_practice
 
-  def initialize(a)
+  def initialize(a, b)
     @test_file = a
+    @test_practice = b
   end
 
 end
